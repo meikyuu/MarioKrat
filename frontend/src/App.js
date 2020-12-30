635f205b571ab7d801e73ba094f7b64af833459c
Daan van der Kallen <mail@daanvdk.com>
2020-12-30 14:57:33 +0100

    Add logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import Loadable from 'react-loadable';
 import Scrollbars from 'react-custom-scrollbars';
 import styled from 'styled-components';
 import theme from './theme';
 import Loader from './component/Loader';
+import Logo from './image/Logo.svg';
 
 function route(path) {
     return Loadable({
@@ -30,12 +31,22 @@ const Wrapper = styled.div`
     padding: 1rem;
 `;
 
+const Title = styled.h1`
+    > img {
+        height: 1em;
+        position: relative;
+        top: 0.125em;
+        margin-right: 0.25em;
+    }
+`;
+
 function App() {
     return (
         <Container>
             <Scrollbars>
                 <Wrapper>
                     <BrowserRouter>
+                        <Title><img src={Logo} alt="logo" />Mario Krat</Title>
                         <Switch>
                             <Route exact path="/" render={(props) => <Home {...props} />} />
                             <Route path="/t/:token?" render={(props) => <Tournament {...props} />} />
