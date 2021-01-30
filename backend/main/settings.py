ed8dcdd3bb26215ab8c91c72f85010d916edc64d
Daan van der Kallen <mail@daanvdk.com>
2021-01-30 14:43:07 +0100

    Fix media

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -109,7 +109,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
+# Media
 
-STATIC_URL = '/static/'
+MEDIA_ROOT = os.environ['MEDIA_ROOT']
